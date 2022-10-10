5f25d70b35ee202185bcd9f12a14f91174ad61cf
KrySeyt <kryseyt@gmail.com>
2022-10-10 20:34:10 +0300

    Heroku db added

--- AsmExplaining/settings.py
+++ AsmExplaining/settings.py
@@ -79,8 +79,8 @@ WSGI_APPLICATION = 'AsmExplaining.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'URL': os.getenv('DATABASE_URL'),
     }
 }
 
--- landing/models.py
+++ landing/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class User(AbstractUser):
+    pass
+
+
+class Article(models.Model):
+    text = models.CharField(max_length=10000, null=True)
+    created_at = models.DateField(default=timezone.now)
+
+
+class DictionaryTerm(models.Model):
+    description = models.CharField(max_length=5000, default="Описание временно отсутствует")
+
+
