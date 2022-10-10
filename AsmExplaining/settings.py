306b6d69f7529af71646eff4bfb2de44dd4c7fba
KrySeyt <kryseyt@gmail.com>
2022-10-10 20:06:35 +0300

    asm-explaining.me to ALLOWED_HOSTS

--- AsmExplaining/settings.py
+++ AsmExplaining/settings.py
@@ -23,11 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DjangoSecretKey')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
-    'asm-explaining.com',
-    'asm-explaining.herokuapp.com',
+    'asm-explaining.me',
     '127.0.0.1',
 ]
 
