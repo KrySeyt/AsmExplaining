75982dc3ca8fd0d8839b46552b46755a30f44bed
KrySeyt <kryseyt@gmail.com>
2022-10-11 04:10:16 +0300

    main updated

--- landing/urls.py
+++ landing/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.main, name='main'),
+    path('flags', views.flags, name='flags'),
 ]
--- landing/views.py
+++ landing/views.py
@@ -2,5 +2,9 @@ from django.http import HttpRequest
 from django.shortcuts import render
 
 
+def flags(request: HttpRequest):
+    return render(request, 'landing/flags.html')
+
+
 def main(request: HttpRequest):
     return render(request, 'landing/main.html')
