7cffdb1350724c91c77311ab1933532b82b7d22a
KrySeyt <kryseyt@gmail.com>
2022-10-11 03:21:39 +0300

    Admin created

--- landing/admin.py
+++ landing/admin.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
 from .models import User, DictionaryTerm
+from .forms import DictionaryTermAdminForm
 
 
+@admin.register(DictionaryTerm)
 class DictionaryTermAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'slug', 'description')
+    form = DictionaryTermAdminForm
 
 
-admin.site.register(DictionaryTerm, DictionaryTermAdmin)
 admin.site.register(User, UserAdmin)
